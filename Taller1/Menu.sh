f492c43480a15412b580fb4c6f9cd14135751c8c
Yesid Briceño <yesidizq@hotmail.com>
2023-03-08 14:53:49 -0500

    Menu

--- Taller1/Menu.sh
+++ Taller1/Menu.sh
@@ -310,10 +310,10 @@ do
                     echo "-----------------------------------------------------------------------------"
                     sudo apt-get update -y
                     sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
-                    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - -y
+                    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add
                     sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" -y
                     sudo apt update -y
-                    sudo apt-cache policy docker-ce -y
+                    sudo apt-cache policy docker-ce 
                     sudo apt install docker-ce -y
 
                     echo "-----------------------------------------------------------------------------"
